e30df29d787af0085f3c4042130028837804384c
jennaly <106183040+jennaly@users.noreply.github.com>
2022-08-05 15:23:42 -0700

    project init

---
+++ index.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+navToggle.style.backgroundImage = "url(/assets/shared/icon-hamburger.svg)";
+
+navToggle.addEventListener('click', () => {
+    const visibility = nav.getAttribute("data-visible");
+    if (visibility == 'false') {
+        nav.setAttribute('data-visible', true);
+        navToggle.setAttribute('data-visible', true);
+        navToggle.style.backgroundImage = "url(/assets/shared/icon-close.svg)";
+ 
+    } else {
+        nav.setAttribute('data-visible', false);
+        navToggle.setAttribute('data-visible', false);
+        navToggle.style.backgroundImage = "url(/assets/shared/icon-hamburger.svg)";
+        
+    }
+})
